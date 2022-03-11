c6e89318308f32479394927cd5d4eb569dd0195c
JKincorperated <73645805+JKincorperated@users.noreply.github.com>
2022-03-11 20:31:28 +0000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='ustore',
+    version='1.0',    
+    description='A libary that makes user managment easier.',
+    url='https://github.com/JKincorperated/ustore/',
+    author='JKinc',
+    license='ustore
+    install_requires=['hashlib',
+                      'json',         
+                      'pyaes',
+                      ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
